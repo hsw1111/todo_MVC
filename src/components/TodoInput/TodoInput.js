b20924241b0eeea4660a95734de64c11ff58df89
hsw <hsw@hsw.com>
2017-12-28 17:11:39 +0800

    切换状态和删除已完成存在一点问题，待修复

--- src/components/TodoInput/TodoInput.js
+++ src/components/TodoInput/TodoInput.js
@@ -7,32 +7,35 @@ export default class TodoInput extends React.Component{
   constructor(props){
     super(props)
     this.state = {
-      itemLists:[],
+
+      todoLists:[],// 待做
+      doLists:[],// 已做
+      lists:[],// 显示
       thing:"",
-      todoLists:[],
-      doLists:[]
     }
   }
   componentWillReceiveProps(nextState){
-    if(nextState.itemLists.length>0){
+    if(nextState.lists.length>0){
       $(".footer").css("display","block")
     }else{
       $(".footer").css("display","none")
     }
-    console.log(11)
+
   }
   // enter事件
   enterEvent(e){
-    if(e.keyCode==13){
-      console.log(this.state.thing)
+    if(e.keyCode==13&&e.target.value.trim()!=""){
 
-      const item = this.state.thing
-      this.state.itemLists.push(item)
+      const item = {
+        isDo:false,
+        thing:this.state.thing
+      }
+      this.state.lists.push(item)
       this.state.todoLists.push(item)
       //添加成功后
       this.setState({
-        itemLists:this.state.itemLists,
         todoLists:this.state.todoLists,
+        lists:this.state.lists,
         thing:''
       })
     }
@@ -47,38 +50,63 @@ export default class TodoInput extends React.Component{
  // 底部选择类型
  findType(e){
     $(e.target).addClass("active").siblings().removeClass("active")
+  
+    const type = $(e.target).data("type")
+
+    if(type==1){
+      this.state.lists = this.state.lists
+      this.setState({
+        lists:this.state.lists
+      })
+    }else if(type==2){
+      this.state.lists = this.state.todoLists
+      this.setState({
+        lists:this.state.lists
+      })
+    }else{
+      this.state.lists = this.state.doLists
+      this.setState({
+        lists:this.state.lists
+      })
+    }
+
  }
 
  // 点击圆圈选择当前thing是否已完成
  itemCheck(e){
-  const index = $(e.target).parent().attr("index")
-  if(e.target.classList.contains("checked")){
-    e.target.classList.remove("checked")
-    $(e.target).siblings("span").removeClass("isChecked")
+   const index = $(e.target).parent().data("index")
+  this.state.lists[index].isDo = !this.state.lists[index].isDo
+  const todoLists = this.state.lists.filter(item=>{
+    return !item.isDo
+  })
+  const doLists = this.state.lists.filter(item=>{
+    return item.isDo
+  })
 
-
-  }else{
-    e.target.classList.add("checked")
-    $(e.target).siblings("span").addClass("isChecked")
-  }
+  this.setState({
+    lists:this.state.lists,
+    todoLists:todoLists,
+    doLists:doLists
+  })
 }
 
 // 点击删除
-delete(index){
- 
-  this.state.itemLists.splice(index,1)
+delete(e){
+ const index = $(e.target).parent().data("index")
+  this.state.lists.splice(index,1)
   this.setState({
-    itemLists:this.state.itemLists
+    lists:this.state.lists
   })
-  console.log(index)
-  $(".list_item").eq(index).find("i").removeClass("checked")
-  $(".list_item").eq(index).find("span").removeClass("isChecked")
-  
+
+   
 } 
 
 // 清空已完成的thing
 clearCompleted(){
- 
+  this.state.doLists.length=0
+  this.setState({
+    doLists:this.state.doLists
+  })
 }
 
 //渲染
@@ -89,28 +117,27 @@ clearCompleted(){
           <span><i className="iconfont">&#xe661;</i></span>
           <input type="text" placeholder="What needs to be done?" value={this.state.thing} onChange={(e)=>{this.handleChange(e)}} onKeyDown={(e)=>{this.enterEvent(e)}}/>
         </div>
-        {/* <TodoLists itemLists={this.state.itemLists}/> */}
         <div className="todo_lists">
           <ul>
-            {this.state.itemLists.length==0?'':this.state.itemLists.map((item,index)=>{
+            {this.state.lists.length==0?'':this.state.lists.map((item,index)=>{
               return (
-                <li key={index} className="list_item">
-                  <i onClick={(e,index)=>{this.itemCheck(e,index)}}></i>
-                  <span>{item}</span>
-                  <b onClick={(index)=>{this.delete(index)}}>×</b>
+                <li key={index} className="list_item" data-index={index}>
+                  <i onClick={(e)=>{this.itemCheck(e)}} className={item.isDo==false?"":'checked'}></i>
+                  <span className={item.isDo==false?"":'isChecked'}>{item.thing}</span>
+                  <b onClick={(e)=>{this.delete(e)}}>×</b>
                 </li>
               )
             })}
           </ul>
         </div>
-        <div className="footer clearfix" style={ this.state.itemLists.length>0?styles.show:styles.hide }>
+        <div className="footer clearfix" style={ this.state.lists.length>0?styles.show:styles.hide }>
           <div className="left">
-            {this.state.itemLists.length-this.state.doLists.length} items left
+            {this.state.todoLists.length} items left
           </div>
           <div className="center">
-            <span className="active" onClick={(e)=>{this.findType(e)}}>All</span>
-            <span  onClick={(e)=>{this.findType(e)}}>Active</span>
-            <span  onClick={(e)=>{this.findType(e)}}>Completed</span>
+            <span className="active" onClick={(e)=>{this.findType(e)}} data-type='1'>All</span>
+            <span  onClick={(e)=>{this.findType(e)}} data-type='2'>Active</span>
+            <span  onClick={(e)=>{this.findType(e)}} data-type='3'>Completed</span>
           </div>
           <div className="bottom" style={this.state.doLists.length>0?styles.show:styles.hide} onClick={()=>{this.clearCompleted()}}>
             Clear completed
